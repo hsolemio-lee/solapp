35cbf15f3a30286130e560a7247b342c87332908
hsolemio-lee <hslyjy@gmail.com>
2021-03-11 16:48:57 +0900

    UploadPage 추가

---
+++ solapp/src/page/UploadPage.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<sol-dropzone></sol-dropzone>
+	</div>
+</template>
+<script>
+
+import SolDropzone from "@/components/SolDropzone";
+
+export default {
+	name: 'uploadPage',
+	components: {SolDropzone},
+	props: {
+	
+	},
+	data() {
+		return {
+		
+		}
+	},
+	created() {
+	
+	},
+	methods: {
+	
+	}
+}
+</script>
+<style lang="scss">
+
+</style>
