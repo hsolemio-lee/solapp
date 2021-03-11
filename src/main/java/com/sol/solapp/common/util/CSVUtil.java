cfdadb0b34d747729961cd60a08cfdf069d66f18
hsolemio-lee <hslyjy@gmail.com>
2021-03-11 11:12:14 +0900

    CSV util 확장성 있도록 변경
    - Generic Type 적용
    - Functional interface 적용

--- src/main/java/com/sol/solapp/common/util/CSVUtil.java
+++ src/main/java/com/sol/solapp/common/util/CSVUtil.java
@@ -2,44 +2,38 @@ package com.sol.solapp.common.util;
 
 import com.sol.solapp.common.exception.ServiceException;
 import com.sol.solapp.common.exception.code.ErrorCode;
-import com.sol.solapp.user.entity.User;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @UtilityClass
 public class CSVUtil {
 
     private final String TYPE = "text/csv";
-    private final String[] HEADER = { "id", "firstname", "lastname", "email" };
 
     public boolean hasCSVFormat(MultipartFile file) {
         return TYPE.equals(file.getContentType());
     }
 
-    public Map<Boolean, List<User>> convertCsvToUser(InputStream is) {
+    public <T> Map<Boolean, List<T>> convertCsvToEntity(InputStream is
+            , Function<CSVRecord,T> convertFunction
+            , Predicate<T> validationRule) {
         try {
             BufferedReader fileReader = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
             CSVParser csvParser = new CSVParser(fileReader, CSVFormat.DEFAULT.withFirstRecordAsHeader().withIgnoreHeaderCase().withTrim());
             return csvParser.getRecords().stream()
-                    .map(csvRecord -> User.builder()
-                            .id("".equals(csvRecord.get(HEADER[0])) ? 0L : Long.parseLong(csvRecord.get(HEADER[0])))
-                            .firstName(csvRecord.get(HEADER[1]))
-                            .lastName(csvRecord.get(HEADER[2]))
-                            .email(csvRecord.get(HEADER[3]))
-                            .build())
-                    .collect(Collectors.partitioningBy(user -> !user.getId().equals(0L)
-                            && !ValidateUtil.isEmpty(user.getFirstName())
-                            && !ValidateUtil.isEmpty(user.getLastName())
-                            && !ValidateUtil.isEmpty(user.getEmail())
-                            && ValidateUtil.isValidEmail(user.getEmail())));
+                    .map(convertFunction)
+                    .collect(Collectors.partitioningBy(validationRule));
         } catch (IOException e) {
             throw new ServiceException(ErrorCode.CSV_PARSE_ERROR);
         }
