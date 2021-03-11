baebfed6c3b27a45b4897df76964d7f9aba2bf9e
hsolemio-lee <hslyjy@gmail.com>
2021-03-11 16:42:40 +0900

    CorsFilter 추가

---
+++ src/main/java/com/sol/solapp/common/filter/CorsFilter.java
@@ -0,0 +1,32 @@
+package com.sol.solapp.common.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    final Logger L = LoggerFactory.getLogger(this.getClass());
+    
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:9090");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept, x-xsrf-token, pd-super-key, authorization"
+                /*"x-requested-with, origin, content-type, accept, x-xsrf-token"*/);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Expose-Headers", "Content-Disposition");
+        chain.doFilter(req, res);
+    }
+    
+    public void init(FilterConfig filterConfig) {}
+    
+    public void destroy() {}
+}
