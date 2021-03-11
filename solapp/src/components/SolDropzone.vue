d0fd6ad35920632012ffa92d9fc2a6881cb047c4
hsolemio-lee <hslyjy@gmail.com>
2021-03-11 16:48:15 +0900

    SolDropzone 추가

---
+++ solapp/src/components/SolDropzone.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<vue-dropzone id="upload"
+		              class="sol-drop-zone"
+		              :options="config"></vue-dropzone>
+	</div>
+</template>
+<script>
+	import vueDropzone from 'vue2-dropzone';
+	
+	export default {
+		name: 'SolDropzone',
+		components: {vueDropzone},
+		props: {
+		
+		},
+		data() {
+			return {
+				config: {
+					url: `${this.$http.defaults.baseUrl}/rest/v1/users/upload"`,
+					headers: {
+						'Cache-Control': '',
+						'X-Requested-With': '',
+					}
+				}
+			}
+		},
+		created() {
+		
+		},
+		methods: {
+		
+		}
+	}
+</script>
+<style>
+	.sol-drop-zone {
+		margin: 20px 20px;
+		background-color: #f7f7f7;
+		border: none;
+		min-height: 300px;
+		min-width: 300px;
+		padding: 0px;
+	}
+</style>
