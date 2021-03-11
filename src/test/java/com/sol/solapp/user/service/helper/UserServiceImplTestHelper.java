1a794b45693fdb94734d8885c6efbc04bcc50336
hsolemio-lee <hslyjy@gmail.com>
2021-03-11 13:43:58 +0900

    UserServiceTestHelper change

--- src/test/java/com/sol/solapp/user/service/helper/UserServiceImplTestHelper.java
+++ src/test/java/com/sol/solapp/user/service/helper/UserServiceImplTestHelper.java
@@ -3,6 +3,12 @@ package com.sol.solapp.user.service.helper;
 import com.sol.solapp.user.entity.User;
 import com.sol.solapp.user.rest.dto.UserDTO;
 import lombok.experimental.UtilityClass;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 @UtilityClass
 public class UserServiceImplTestHelper {
@@ -12,16 +18,29 @@ public class UserServiceImplTestHelper {
                 .id(id)
                 .firstName("sol")
                 .lastName("lee")
-                .email("test@test.com")
+                .email("test"+id+"@test.com")
                 .build();
     }
 
-    public User getMockUser(Long id) {
+    public User getMockCreatedUser(Long id) {
         return User.builder()
                 .id(id)
                 .firstName("sol")
                 .lastName("lee")
-                .email("test@test.com")
+                .email("test"+id+"@test.com")
+                .createdTimestamp(new Date())
+                .lastModifiedTimestamp(new Date())
                 .build();
+    }
+
+    public List<User> getMockUserList(long size) {
+        return LongStream.range(1, size+1)
+                .mapToObj(UserServiceImplTestHelper::getMockCreatedUser)
+                .collect(Collectors.toList());
+    }
+
+    public MockMultipartFile getMockMultipartFile(String type, String content) {
+        return new MockMultipartFile("user-file","test.csv",
+                type, content.getBytes());
     }
 }
